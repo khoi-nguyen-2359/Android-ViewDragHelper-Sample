eff1770606c9caef5c7fe732ca08aada023e67ed
Khoi Nguyen <khoi.nguyenanh@2359media.com.vn>
2014-10-02 15:59:22 +0700

    no message

--- src/com/flavienlaurent/vdh/MyLayout.java
+++ src/com/flavienlaurent/vdh/MyLayout.java
@@ -1,5 +1,11 @@
 package com.flavienlaurent.vdh;
 
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringConfig;
+import com.facebook.rebound.SpringSystem;
+import com.facebook.rebound.SpringUtil;
+
 import android.content.Context;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
@@ -17,6 +23,7 @@ public class MyLayout extends ViewGroup {
 
 		@Override
 		public boolean tryCaptureView(View paramView, int paramInt) {
+			spring.setEndValue(1);
 			return true;
 		}
 
@@ -24,31 +31,35 @@ public class MyLayout extends ViewGroup {
 		public void onViewPositionChanged(View changedView, int left, int top,
 				int dx, int dy) {
 			super.onViewPositionChanged(changedView, left, top, dx, dy);
-//			mTop = top;
-//			mLeft = left;
+			// mTop = top;
+			// mLeft = left;
 		}
 
 		@Override
-	    public int clampViewPositionVertical(View child, int top, int dy) {
-			Log.d("khoi", "clamp top="+top);
+		public int clampViewPositionVertical(View child, int top, int dy) {
+			Log.d("khoi", "clamp top=" + top);
 			return mTop = top;
-	    }
-		
+		}
+
 		@Override
 		public int clampViewPositionHorizontal(View child, int left, int dx) {
-			Log.d("khoi", "clamp left="+left);
+			Log.d("khoi", "clamp left=" + left);
 			return mLeft = left;
 		}
 
 		@Override
 		public void onViewReleased(View releasedChild, float xvel, float yvel) {
 			super.onViewReleased(releasedChild, xvel, yvel);
-			Log.d("khoi", "xvel="+xvel+",yvel="+yvel);
+			spring.setEndValue(0);
+
+			Log.d("khoi", "xvel=" + xvel + ",yvel=" + yvel);
 			final float dur = 0.5f;
 			float finLeft = xvel * dur + releasedChild.getLeft();
 			float finTop = yvel * dur + releasedChild.getTop();
-			
-			dragHelper.flingCapturedView(0, 0, getWidth()-releasedChild.getWidth(), getHeight()-releasedChild.getHeight());
+
+			dragHelper.flingCapturedView(0, 0,
+					getWidth() - releasedChild.getWidth(), getHeight()
+							- releasedChild.getHeight());
 			ViewCompat.postInvalidateOnAnimation(MyLayout.this);
 		}
 	}
@@ -58,19 +69,49 @@ public class MyLayout extends ViewGroup {
 
 	private int mTop;
 	private int mLeft;
-	
+
 	private int endX;
 	private int endY;
-	
+	private Spring spring;
+
 	public MyLayout(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init();
 	}
 
 	private void init() {
-		dragHelper = ViewDragHelper.create(this, 0.5f, new DragCallback());
+		dragHelper = ViewDragHelper.create(this, 0.75f, new DragCallback());
 		endX = 0;
 		endY = 0;
+
+		// Create a system to run the physics loop for a set of springs.
+		SpringSystem springSystem = SpringSystem.create();
+
+		// Add a spring to the system.
+		spring = springSystem.createSpring();
+
+		// Add a listener to observe the motion of the spring.
+		spring.addListener(new SimpleSpringListener() {
+
+			@Override
+			public void onSpringUpdate(Spring spring) {
+				// You can observe the updates in the spring
+				// state by asking its current value in onSpringUpdate.
+				float mappedValue = (float) SpringUtil
+						.mapValueFromRangeToRange(spring.getCurrentValue(), 0,
+								1, 1, 0.8);
+				squareView.setScaleX(mappedValue);
+				squareView.setScaleY(mappedValue);
+			}
+		});
+
+		// Set the spring in motion; moving from 0 to 1
+		// spring.setEndValue(1);
+		SpringConfig springConfig = spring.getSpringConfig();
+//		 springConfig.friction = 10;
+		springConfig.tension = 1000;
+		Log.d("khoi", "tension=" + springConfig.tension + ",friction="
+				+ springConfig.friction);
 	}
 
 	public MyLayout(Context context, AttributeSet attrs) {
@@ -82,16 +123,16 @@ public class MyLayout extends ViewGroup {
 		super(context);
 		init();
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		dragHelper.processTouchEvent(event);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return dragHelper.shouldInterceptTouchEvent(ev);
+		return dragHelper.shouldInterceptTouchEvent(ev);
 	}
 
 	@Override
@@ -99,29 +140,34 @@ public class MyLayout extends ViewGroup {
 		super.onFinishInflate();
 
 		squareView = findViewById(R.id.view_square);
-//		squareView.setOnTouchListener(new OnTouchListener() {
-//			@Override
-//			public boolean onTouch(View v, MotionEvent event) {
-//				return false;
-//			}
-//		});
-		
-//		squareView.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				int left = endX * (getWidth() - squareView.getWidth());
-//				int top = endY * (getHeight() - squareView.getHeight());
-//				
-////				Log.d("khoi", "l="+left+",t="+top);
-////				dragHelper.smoothSlideViewTo(squareView, left, top);
-////				ViewCompat.postInvalidateOnAnimation(MyLayout.this);
-//				
-//				endX = (endX + 1) % 2;
-//				endY = (endY + 1) % 2;
-//			}
-//		});
+		// squareView.setOnTouchListener(new OnTouchListener() {
+		// @Override
+		// public boolean onTouch(View v, MotionEvent event) {
+		// int action = event.getAction();
+		// switch (action) {
+		// case MotionEvent.ACTION_DOWN:
+		// spring.setEndValue(1);
+		// break;
+		// case MotionEvent.ACTION_UP:
+		// spring.setEndValue(0);
+		// break;
+		// case MotionEvent.ACTION_MOVE:
+		// //
+		// squareView.offsetLeftAndRight((int)event.getX()-squareView.getWidth()/2-squareView.getLeft());
+		// //
+		// squareView.offsetTopAndBottom((int)event.getY()-squareView.getHeight()/2-squareView.getTop());
+		// // spring.setEndValue(0);
+		// break;
+		// default:
+		// break;
+		// }
+		//
+		// return true;
+		// }
+		// });
+
 	}
-	
+
 	@Override
 	public void computeScroll() {
 		if (dragHelper.continueSettling(true)) {
@@ -131,17 +177,19 @@ public class MyLayout extends ViewGroup {
 
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		squareView.layout(mLeft, mTop, mLeft + squareView.getMeasuredWidth(), mTop + squareView.getMeasuredHeight());
+		squareView.layout(mLeft, mTop, mLeft + squareView.getMeasuredWidth(),
+				mTop + squareView.getMeasuredHeight());
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		measureChildren(widthMeasureSpec, heightMeasureSpec);
 
-        int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int maxHeight = MeasureSpec.getSize(heightMeasureSpec);
+		int maxWidth = MeasureSpec.getSize(widthMeasureSpec);
+		int maxHeight = MeasureSpec.getSize(heightMeasureSpec);
 
-        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, 0),
-                resolveSizeAndState(maxHeight, heightMeasureSpec, 0));
+		setMeasuredDimension(
+				resolveSizeAndState(maxWidth, widthMeasureSpec, 0),
+				resolveSizeAndState(maxHeight, heightMeasureSpec, 0));
 	}
 }
